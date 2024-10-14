e9edffb8dbf5d65827cec17cee7b19f7ede5b81d
feuchmor <feuchmor@students.zhaw.ch>
2024-10-14 23:19:57 +0200

    STS quiz solver 4, task 9 and 10

--- 3-semester/stochastik-statistik/quiz-solver/quiz-4-multivariate-statistics/quiz-4-solver.py
+++ 3-semester/stochastik-statistik/quiz-solver/quiz-4-multivariate-statistics/quiz-4-solver.py
@@ -24,9 +24,9 @@ sorted_x_coordinates_str = hf.list_to_no_space_string(sorted_x_coordinates)
 print('(2) sorted x-Coordinates:', sorted_x_coordinates_str)
 
 # 3: ranking list of x-Coordinates:
-ranking_list = hf.get_ranking_list(x_coordinates)
-ranking_list_str = hf.list_to_no_space_string(ranking_list)
-print('(3) ranking list of x-Coordinates:', ranking_list_str)
+ranking_list_x = hf.get_ranking_list(x_coordinates)
+ranking_list_x_str = hf.list_to_no_space_string(ranking_list_x)
+print('(3) ranking list of x-Coordinates:', ranking_list_x_str)
 
 
 # 4: y-Coordinates:
@@ -40,9 +40,9 @@ sorted_y_coordinates_str = hf.list_to_no_space_string(sorted_y_coordinates)
 print('(5) sorted y-Coordinates:', sorted_y_coordinates_str)
 
 # 6: ranking list of y-Coordinates:
-ranking_list = hf.get_ranking_list(y_coordinates)
-ranking_list_str = hf.list_to_no_space_string(ranking_list)
-print('(6) ranking list of y-Coordinates:', ranking_list_str)
+ranking_list_y = hf.get_ranking_list(y_coordinates)
+ranking_list_y_str = hf.list_to_no_space_string(ranking_list_y)
+print('(6) ranking list of y-Coordinates:', ranking_list_y_str)
 
 # 7: Covariance (not corrected):
 mean_x = sum(x_coordinates) / len(x_coordinates)
@@ -59,5 +59,31 @@ variance_y = sum([(y - mean_y) ** 2 for y in y_coordinates]) / len(y_coordinates
 pearsons_correlation_coefficient = covariance / sqrt(variance_x * variance_y)
 
 print('(8) Pearson\'s correlation coefficient:', round(pearsons_correlation_coefficient, number_of_decimal_places))
+
+# 9: Covariance of the ranking lists (not corrected):
+
+mean_ranking_x = sum(ranking_list_x) / len(ranking_list_x)
+mean_ranking_y = sum(ranking_list_y) / len(ranking_list_y)
+covariance_ranking_lists = sum([(x - mean_ranking_x) * (y - mean_ranking_y) for x, y in zip(ranking_list_x, ranking_list_y)]) / len(ranking_list_x)
+
+print('(9) Covariance of the ranking lists (not corrected):', round(covariance_ranking_lists, number_of_decimal_places))
+
+# 10: Spearman's correlation coefficient
+
+# Variance of the ranking lists (standard deviation squared)
+variance_ranking_x = sum([(x - mean_ranking_x) ** 2 for x in ranking_list_x]) / len(ranking_list_x)
+variance_ranking_y = sum([(y - mean_ranking_y) ** 2 for y in ranking_list_y]) / len(ranking_list_y)
+
+# Standard deviation is the square root of the variance
+std_dev_ranking_x = sqrt(variance_ranking_x)
+std_dev_ranking_y = sqrt(variance_ranking_y)
+
+# Spearman's correlation coefficient (r_s)
+spearman_correlation_coefficient = covariance_ranking_lists / (std_dev_ranking_x * std_dev_ranking_y)
+
+print('(10) Spearman\'s correlation coefficient:', round(spearman_correlation_coefficient, number_of_decimal_places))
+
+
+
 
 
