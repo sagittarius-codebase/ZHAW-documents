3b940e72fd8704207aaaf53da0b7921b21b80ead
feuchmor <feuchmor@students.zhaw.ch>
2024-10-14 23:07:50 +0200

    STS quiz solver 4, task 7

--- 3-semester/stochastik-statistik/quiz-solver/quiz-4-multivariate-statistics/quiz-4-solver.py
+++ 3-semester/stochastik-statistik/quiz-solver/quiz-4-multivariate-statistics/quiz-4-solver.py
@@ -44,7 +44,13 @@ ranking_list = hf.get_ranking_list(y_coordinates)
 ranking_list_str = hf.list_to_no_space_string(ranking_list)
 print('(6) ranking list of y-Coordinates:', ranking_list_str)
 
+# 7: Covariance (not corrected):
+mean_x = sum(x_coordinates) / len(x_coordinates)
+mean_y = sum(y_coordinates) / len(y_coordinates)
 
+covariance = sum([(x - mean_x) * (y - mean_y) for x, y in bivariate_data]) / len(bivariate_data)
+
+print('(7) Covariance (not corrected):', round(covariance, number_of_decimal_places))
 
 
 
