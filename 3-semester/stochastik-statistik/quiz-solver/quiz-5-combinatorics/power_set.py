13311efe79ed555d0a8deb833c4d9d30db3ea1f5
feuchmor <feuchmor@students.zhaw.ch>
2024-10-21 10:14:02 +0200

    moved function to delete redundant file

--- 3-semester/stochastik-statistik/quiz-solver/quiz-5-combinatorics/power_set.py
+++ 3-semester/stochastik-statistik/quiz-solver/quiz-5-combinatorics/power_set.py
@@ -1,5 +1,3 @@
-import helper_functions as hf
-
 
 def power_set_of_set(my_set):
     """
@@ -21,6 +19,24 @@ def power_set_of_set(my_set):
         for subset in power_set:
             power_set = power_set + [list(subset) + [element]]
 
-    power_set_string = hf.convert_to_string(power_set)
+    power_set_string = convert_to_string(power_set)
 
     return power_set_string
+
+
+def convert_to_string(power_set):
+    """
+    This function converts a power set to a string.
+    Form: "{{},{x},{y},{z},{x,y},{x,z},{y,z},{x,y,z}}"
+
+    :param list power_set: power set to be converted to string
+    :return: power set in correct form
+    :rtype: string
+    """
+
+    result = "{"
+    for subset in power_set:
+        subset_str = "{" + ",".join(map(str, subset)) + "}"
+        result += subset_str + ","
+    result = result[:-1] + "}"  # Remove the last comma and space, then close the outer braces
+    return result
