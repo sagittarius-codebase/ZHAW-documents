4a6a74d03ea77ec45182f5d8a95ee0fd6e012557
feuchmor <feuchmor@students.zhaw.ch>
2024-10-31 15:39:03 +0100

    STS week 07 calc functions

---
+++ 3-semester/stochastik-statistik/quiz-solver/quiz-7-elementary-probability-(characteristics)/calc_characteristics.py
@@ -0,0 +1,16 @@
+
+def get_expected_val(x, pmf):
+    """
+    Calculate the expected value of a random variable.
+
+    :param list x: list of values of the random variable
+    :param list pmf: list of the probability mass function values
+    :return: the expected value of the random variable
+    :rtype: float
+    """
+    expected_val = 0
+    for i in range(len(x)):
+        if isinstance(pmf[i], str):
+            pmf[i] = eval(pmf[i])
+        expected_val += x[i] * pmf[i]
+    return int(expected_val * 10000) / 10000
