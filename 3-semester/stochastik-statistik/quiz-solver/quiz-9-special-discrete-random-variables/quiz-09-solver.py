5fc35a0541f8e692b850555fb56dca47c588caee
feuchmor <feuchmor@students.zhaw.ch>
2024-11-25 08:39:07 +0100

    STS quiz-09 DONE

--- 3-semester/stochastik-statistik/quiz-solver/quiz-9-special-discrete-random-variables/quiz-09-solver.py
+++ 3-semester/stochastik-statistik/quiz-solver/quiz-9-special-discrete-random-variables/quiz-09-solver.py
@@ -5,7 +5,7 @@
 
 # Imports:
 from scipy.stats import poisson
-from math import log, ceil
+from math import log, ceil, comb
 
 
 ####################################################################################################
@@ -26,6 +26,7 @@ print("The probability of the maximum of requests in", number_of_minutes, "minut
 # Exercise 2: probabilistic program
 ####################################################################################################
 
+# Replace the following with the values of your quiz:
 correct_result_prob = 0.28
 minimum_prob_for_correct_result = 0.93
 
@@ -41,4 +42,45 @@ minimum_prob_for_correct_result = 0.93
 n = ceil(log(1 - minimum_prob_for_correct_result) / log(1 - correct_result_prob))
 
 print("The minimum number of repetitions needed to reach the minimum probability for a correct result is:", n, "\n")
+
+
+####################################################################################################
+# Exercise 3: Lottery with luck-tickets
+####################################################################################################
+
+# Replace the following with the values of your quiz:
+number_of_tickets = 16 # N
+number_of_luck_tickets = 8 # M
+number_purchased_tickets = 7 # n
+maximum_of_received_luck_tickets = 1 # if text sais: less than 2, then 1!
+minimum_of_received_luck_tickets = 0
+# based on your text set either maximum_of_received_luck_tickets or minimum_of_received_luck_tickets
+# the other one should be set to 0
+
+# Calculate the probability of getting at most the MAXIMUM of defined luck-tickets:
+if minimum_of_received_luck_tickets == 0:
+    probability = 0
+    for i in range(maximum_of_received_luck_tickets + 1):
+        probability += comb(number_of_luck_tickets, i) * comb(number_of_tickets - number_of_luck_tickets, number_purchased_tickets - i) / comb(number_of_tickets, number_purchased_tickets)
+    print("The probability of getting at most the MAXIMUM of defined luck-tickets is:", round(probability, 3), "\n")
+else:
+    probability = 0
+    for i in range(minimum_of_received_luck_tickets, number_purchased_tickets + 1):
+        probability += comb(number_of_luck_tickets, i) * comb(number_of_tickets - number_of_luck_tickets, number_purchased_tickets - i) / comb(number_of_tickets, number_purchased_tickets)
+    probability = 1 - probability
+    print("The probability of getting at least the MINIMUM of defined luck-tickets is:", round(probability, 3), "\n")
+
+
+####################################################################################################
+# Exercise 4: Number is binomial distributed
+####################################################################################################
+
+# Replace the following with the values of your quiz:
+number_of_trials = 6 # n
+probability_of_success = 0.4 # p
+number_of_successes = 3 # x
+
+P = comb(number_of_trials, number_of_successes) * probability_of_success**number_of_successes * (1 - probability_of_success)**(number_of_trials - number_of_successes)
+
+print("The probability of getting", number_of_successes, "successes in", number_of_trials, "trials is:", round(P, 3), "\n")
 
