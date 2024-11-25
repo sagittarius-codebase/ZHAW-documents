01d75da9bd4786576a8a19699af118b0564f8300
feuchmor <feuchmor@students.zhaw.ch>
2024-11-25 22:44:53 +0100

    STS quiz-9 my solutions

--- 3-semester/stochastik-statistik/quiz-solver/quiz-9-special-discrete-random-variables/quiz-09-solver.py
+++ 3-semester/stochastik-statistik/quiz-solver/quiz-9-special-discrete-random-variables/quiz-09-solver.py
@@ -13,13 +13,13 @@ from math import log, ceil, comb
 ####################################################################################################
 
 # Replace the following with the values of your quiz:
-requests_per_minute = 8697 / 100
-number_of_minutes = 30
-maximum_of_requests = 2629
+requests_per_minute = 273 / 20
+number_of_minutes = 10
+maximum_of_requests = 154
 
 # Calculate the probability of the MAXIMUM of requests in 30 minutes:
 P = poisson.cdf(maximum_of_requests, requests_per_minute * number_of_minutes)
-print("The probability of the maximum of requests in", number_of_minutes, "minutes is:", round(P, 3), "\n")
+print("The probability of the maximum of requests in", number_of_minutes, "minutes is:", round(P, 4), "\n")
 
 
 ####################################################################################################
@@ -27,8 +27,8 @@ print("The probability of the maximum of requests in", number_of_minutes, "minut
 ####################################################################################################
 
 # Replace the following with the values of your quiz:
-correct_result_prob = 0.28
-minimum_prob_for_correct_result = 0.93
+correct_result_prob = 0.31
+minimum_prob_for_correct_result = 0.94
 
 # Calculate the minimum number of repetitions needed to reach the minimum probability for a correct result:
 # Actual equation: (0.28 = p, 0.93 = P, n = number of repetitions)
@@ -49,26 +49,30 @@ print("The minimum number of repetitions needed to reach the minimum probability
 ####################################################################################################
 
 # Replace the following with the values of your quiz:
-number_of_tickets = 16 # N
+number_of_tickets = 36 # N
 number_of_luck_tickets = 8 # M
-number_purchased_tickets = 7 # n
-maximum_of_received_luck_tickets = 1 # if text sais: less than 2, then 1!
+number_purchased_tickets = 5 # n
+maximum_of_received_luck_tickets = 0 # if text sais: less than 2, then 1!
 minimum_of_received_luck_tickets = 0
+exactly_received_luck_tickets = 2
 # based on your text set either maximum_of_received_luck_tickets or minimum_of_received_luck_tickets
 # the other one should be set to 0
 
 # Calculate the probability of getting at most the MAXIMUM of defined luck-tickets:
-if minimum_of_received_luck_tickets == 0:
+if minimum_of_received_luck_tickets == 0 and exactly_received_luck_tickets == 0:
     probability = 0
     for i in range(maximum_of_received_luck_tickets + 1):
         probability += comb(number_of_luck_tickets, i) * comb(number_of_tickets - number_of_luck_tickets, number_purchased_tickets - i) / comb(number_of_tickets, number_purchased_tickets)
     print("The probability of getting at most the MAXIMUM of defined luck-tickets is:", round(probability, 3), "\n")
-else:
+elif maximum_of_received_luck_tickets == 0 and exactly_received_luck_tickets == 0:
     probability = 0
     for i in range(minimum_of_received_luck_tickets, number_purchased_tickets + 1):
         probability += comb(number_of_luck_tickets, i) * comb(number_of_tickets - number_of_luck_tickets, number_purchased_tickets - i) / comb(number_of_tickets, number_purchased_tickets)
     probability = 1 - probability
     print("The probability of getting at least the MINIMUM of defined luck-tickets is:", round(probability, 3), "\n")
+else:
+    probability = comb(number_of_luck_tickets, exactly_received_luck_tickets) * comb(number_of_tickets - number_of_luck_tickets, number_purchased_tickets - exactly_received_luck_tickets) / comb(number_of_tickets, number_purchased_tickets)
+    print("The probability of getting exactly the DEFINED number of luck-tickets is:", round(probability, 3), "\n")
 
 
 ####################################################################################################
@@ -77,7 +81,7 @@ else:
 
 # Replace the following with the values of your quiz:
 number_of_trials = 6 # n
-probability_of_success = 0.4 # p
+probability_of_success = 0.2 # p
 number_of_successes = 3 # x
 
 P = comb(number_of_trials, number_of_successes) * probability_of_success**number_of_successes * (1 - probability_of_success)**(number_of_trials - number_of_successes)
