e1d1a92a60166c6b6a3bdda62395cb48053a0a25
feuchmor <feuchmor@students.zhaw.ch>
2024-11-25 22:27:01 +0100

    STS quiz-10 DONE (now actually)

--- 3-semester/stochastik-statistik/quiz-solver/quiz-10-normal-distribution/quiz-10-solver.py
+++ 3-semester/stochastik-statistik/quiz-solver/quiz-10-normal-distribution/quiz-10-solver.py
@@ -20,7 +20,57 @@ upper_bound = expected_value + 3 * standard_deviation
 
 print("The intervall that contains 99.7% of the values is:", lower_bound, "to", upper_bound, "\n")
 
-# The rest of the exercises use a table for the normal distribution, which is not available in Python.
-# Therefore, the rest of the exercises are not solvable with Python.
+
+####################################################################################################
+# Exercise 2: Standard Normal Distribution
+####################################################################################################
+
+# Replace the following with the values of your quiz:
+value = 1.87 # value x in: P(X <= x)
+
+print("Search in your table for the value and determine the probability that the value is less or equal to.")
+print("value = ", value, "\n")
+
+####################################################################################################
+# Exercise 3: Normal Distribution with custom expected value and standard deviation
+####################################################################################################
+
+# Replace the following with the values of your quiz:
+expected_value = 40 # μ
+standard_deviation = 10 # σ
+value = 63
+
+# determine the probability that the value is less or equal to 63:
+z = (value - expected_value) / standard_deviation
+
+print("Search in your table for the value of z and determine the probability that the value is less or equal to ", value)
+print("z = ", z, "\n")
+
+####################################################################################################
+# Exercise 4: Standard Normal Distribution with higher than rather than less or equal
+####################################################################################################
+
+# Replace the following with the values of your quiz:
+value = 2.89 # value x in: P(X > x)
+
+print("Search in your table for the value and determine the probability that the value is higher than.")
+print("value = ", value)
+print("An then subtract the value from 1 to get the probability that the value is higher than. -> P(X > x) = 1 - P(X <= x)")
+
+####################################################################################################
+# Exercise 5: general standardization of a normal distribution
+####################################################################################################
+
+# Replace the following with the values of your quiz:
+expected_value = -1 # μ
+standard_deviation = 3 # σ
+
+# How to calculate U out of X generally?
+
+print("U = (X - μ) / σ, so here:")
+if expected_value < 0:
+    print("U = ( X +", abs(expected_value), ") /", standard_deviation)
+else:
+    print("U = ( X -", expected_value, ") /", standard_deviation)
 
 
