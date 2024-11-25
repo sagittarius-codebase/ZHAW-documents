3583bcef017995eca5c9bd45b3a20ada30436d60
feuchmor <feuchmor@students.zhaw.ch>
2024-11-25 08:57:23 +0100

    STS quiz-10 DONE

---
+++ 3-semester/stochastik-statistik/quiz-solver/quiz-10-normal-distribution/quiz-10-solver.py
@@ -0,0 +1,26 @@
+
+####################################################################################################
+# Quiz 10: Normal Distribution
+####################################################################################################
+
+# Imports:
+
+####################################################################################################
+# Exercise 1: Normal Distribution Intervall
+####################################################################################################
+
+# Replace the following with the values of your quiz:
+expected_value = 70 # μ
+standard_deviation = 2 # σ
+
+
+# determine the intervall that contains 99.7% of the values:
+lower_bound = expected_value - 3 * standard_deviation
+upper_bound = expected_value + 3 * standard_deviation
+
+print("The intervall that contains 99.7% of the values is:", lower_bound, "to", upper_bound, "\n")
+
+# The rest of the exercises use a table for the normal distribution, which is not available in Python.
+# Therefore, the rest of the exercises are not solvable with Python.
+
+
