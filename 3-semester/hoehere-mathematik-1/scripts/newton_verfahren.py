1922cf0ca98abe72464b8df8ca235e06615320cd
feuchmor <feuchmor@students.zhaw.ch>
2025-01-15 00:32:17 +0100

    refactored for better reuse

--- 3-semester/hoehere-mathematik-1/scripts/newton_verfahren.py
+++ 3-semester/hoehere-mathematik-1/scripts/newton_verfahren.py
@@ -1,28 +1,35 @@
 import sympy as sp
 
-# replace with your values:
-x = sp.Symbol('x')
-f_x = (x ** 2) - 2
-f_prime_x = sp.diff(f_x, x)
+def newton_verfahren(f_x, f_prime_x, start_value):
+    # Newton's method implementation
+    if f_prime_x.subs(x, start_value).evalf() == 0:
+        raise ValueError("Ableitung ist Null an der Stelle x = {}".format(start_value))
 
-start_value = 1
+    x_n = start_value
+    for i in range(MAX_ITERATIONS):
+        f_x_n = f_x.subs(x, x_n).evalf()
+        f_prime_x_n = f_prime_x.subs(x, x_n).evalf()
+        x_n_next = x_n - f_x_n / f_prime_x_n
+        print("Iteration {}: x_n = {}".format(i + 1, x_n_next))
+        if abs(x_n_next) < EPSILON_ZERO_VALUE or abs(x_n_next - x_n) < EPSILON_TWO_VALUES:
+            break
+        x_n = x_n_next
 
-MAX_ITERATIONS = 1000
-EPSILON_ZERO_VALUE = 1e-6
-EPSILON_TWO_VALUES = 1e-6
+    print("\nNewtonverfahren - Nullstelle (geschätzt):", x_n)
 
-# Newton's method implementation
-if f_prime_x.subs(x, start_value).evalf() == 0:
-    raise ValueError("Ableitung ist Null an der Stelle x = {}".format(start_value))
+    return x_n
 
-x_n = start_value
-for i in range(MAX_ITERATIONS):
-    f_x_n = f_x.subs(x, x_n).evalf()
-    f_prime_x_n = f_prime_x.subs(x, x_n).evalf()
-    x_n_next = x_n - f_x_n / f_prime_x_n
-    print("Iteration {}: x_n = {}".format(i + 1, x_n_next))
-    if abs(x_n_next) < EPSILON_ZERO_VALUE or abs(x_n_next - x_n) < EPSILON_TWO_VALUES:
-        break
-    x_n = x_n_next
 
-print("\nNewtonverfahren - Nullstelle (geschätzt):", x_n)
+if __name__ == "__main__":
+    # replace with your values:
+    x = sp.Symbol('x')
+    f_x = (x ** 2) - 2
+    f_prime_x = sp.diff(f_x, x)
+
+    start_value = 1
+
+    MAX_ITERATIONS = 1000
+    EPSILON_ZERO_VALUE = 1e-6
+    EPSILON_TWO_VALUES = 1e-6
+
+    newton_verfahren(f_x, f_prime_x, start_value)
