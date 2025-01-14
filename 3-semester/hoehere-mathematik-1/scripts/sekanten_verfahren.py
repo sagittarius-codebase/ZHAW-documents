41f859e17b17f5f9cfa5aaa90fa933f661934a05
feuchmor <feuchmor@students.zhaw.ch>
2025-01-15 00:33:12 +0100

    refactored for better reuse

--- 3-semester/hoehere-mathematik-1/scripts/sekanten_verfahren.py
+++ 3-semester/hoehere-mathematik-1/scripts/sekanten_verfahren.py
@@ -1,30 +1,38 @@
 import sympy as sp
 
-# replace with your values:
-x = sp.Symbol('x')
-f_x = (x ** 2) - 2
 
-# two starting values
-start_value_1 = 1
-start_value_2 = 2
+def sekanten_verfahren(f_x, start_value_1, start_value_2):
+    # Sekanten-Verfahren implementation
+    x_n_minus_1 = start_value_1
+    x_n = start_value_2
 
-MAX_ITERATIONS = 1000
-EPSILON_ZERO_VALUE = 1e-6
-EPSILON_TWO_VALUES = 1e-6
+    for i in range(MAX_ITERATIONS):
+        f_x_n_minus_1 = f_x.subs(x, x_n_minus_1).evalf()
+        f_x_n = f_x.subs(x, x_n).evalf()
 
-# Sekanten-Verfahren implementation
-x_n_minus_1 = start_value_1
-x_n = start_value_2
+        x_n_plus_1 = x_n - f_x_n * ((x_n - x_n_minus_1) / (f_x_n - f_x_n_minus_1))
+        print("Iteration {}: x_n = {}".format(i + 1, x_n_plus_1))
+        if abs(x_n_plus_1 - x_n) < EPSILON_ZERO_VALUE or abs(x_n_plus_1 - x_n) < EPSILON_TWO_VALUES:
+            break
+        x_n_minus_1 = x_n
+        x_n = x_n_plus_1
 
-for i in range(MAX_ITERATIONS):
-    f_x_n_minus_1 = f_x.subs(x, x_n_minus_1).evalf()
-    f_x_n = f_x.subs(x, x_n).evalf()
+    print("\nSekantenverfahren - Nullstelle (geschätzt):", x_n)
 
-    x_n_plus_1 = x_n - f_x_n * ((x_n - x_n_minus_1) / (f_x_n - f_x_n_minus_1))
-    print("Iteration {}: x_n = {}".format(i + 1, x_n_plus_1))
-    if abs(x_n_plus_1 - x_n) < EPSILON_ZERO_VALUE or abs(x_n_plus_1 - x_n) < EPSILON_TWO_VALUES:
-        break
-    x_n_minus_1 = x_n
-    x_n = x_n_plus_1
+    return x_n
 
-print("\nSekantenverfahren - Nullstelle (geschätzt):", x_n)
+
+if __name__ == "__main__":
+    # replace with your values:
+    x = sp.Symbol('x')
+    f_x = (x ** 2) - 2
+
+    # two starting values
+    start_value_1 = 1
+    start_value_2 = 2
+
+    MAX_ITERATIONS = 1000
+    EPSILON_ZERO_VALUE = 1e-6
+    EPSILON_TWO_VALUES = 1e-6
+
+    sekanten_verfahren(f_x, start_value_1, start_value_2)
